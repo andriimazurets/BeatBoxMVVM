f9ea1d9588a8fc996aa97565cb36897392c02a84
theri <therigasmazurets@gmail.com>
2023-11-29 13:11:04 +0200

    up

--- app/src/main/java/com/example/beatboxmvvm/MainActivity.kt
+++ app/src/main/java/com/example/beatboxmvvm/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.beatboxmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.beatboxmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
